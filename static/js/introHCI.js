d9bbb6e59615419f9ab2a390ed683e7e1c37095e
Aaron Liu <a3liu@ucsd.edu>
2018-01-25 11:21:35 -0800

    Added the js so that the buttons toggle descriptions and text color

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -13,6 +13,34 @@ function initializePage() {
 		$('.jumbotron h1').text("Javascript is connected");
 	});
 
+    $("a.thumbnail").click(projectClick);
+    $("#testjs").click(function(e) {
+        $(".jumbotron h1").text("Assuming direct control");
+        $("#testjs").text("Please wait...");
+        $(".jumbotron p").toggleClass("active");
+    });
+
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e){
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject.find(".project-description"));
+
+    if(description.length == 0) {   
+        containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+
+        $(".project-description").fadeIn();
+    }
+
+    else {
+        /*description.html("<p>Stop clicking on me! You just did it at " + 
+        (new Date()) + "</p>");*/
+
+        $(".project-description").fadeToggle();
+    }
 }
